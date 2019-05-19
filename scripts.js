c6e97bc949bb3bd9825df00ba8be891416a4b7ef
Sejla Jahic <sejla_jahic@hotmail.com>
2019-05-19 10:14:53 +0200

    First

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    document.getElementById("contentPersonalInfo").style.visibility = "visible";
+  }
+
+function myFunction1(){
+    document.getElementById("contentEducation").style.visibility = "visible";
+ }
+
+function myFunction2(){
+    document.getElementById("contentSkills").style.visibility = "visible";
+ }
+
+function myFunction3(){
+    document.getElementById("contentProjects").style.visibility = "visible";
+ }
+
+function myFunction4(){
+    document.getElementById("contentWorkingHistory").style.visibility = "visible";
+ }
